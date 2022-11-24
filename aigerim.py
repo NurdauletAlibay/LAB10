dfc7f9ec1669f062b2f87bcf4b3165d5d1121d37
AigerimNurmakhanova <31aigerim.nurmahan@mail.ru>
2022-11-24 12:35:50 +0600

    Aigerim

--- aigerim.py
+++ aigerim.py
@@ -1,59 +1,105 @@
 ...
-1) one_iterable = [2, 1, 3, 4, 7, 11]
-   another_iterable = ['P', 'y', 't', 'h', 'o', 'n']
-   for n, letter in zip(one_iterable, another_iterable):     
-   print(letter, n)
+1) numbers = [1,2,3]
+str_numbers = ['One','Two','Three']
+roman_numbers = ['I','II','III']
+result = zip(numbers, str_numbers, roman_numbers)
+print(result)
+print(list(result))
+
+ответ:[(1, 'One', 'I'), (2, 'Two', 'II'), (3, 'Three', 'III')]
 ...
 
 ...
-2) def palindromic(sequence):
-    """Возвращает True, если последовательность является палиндромом."""
-    for n, m in zip(sequence, reversed(sequence)):
-        if n != m:
-            return False
-    return True
+2) py_string = 'Python'
+
+# stop = 3
+# contains 0, 1 and 2 indices
+slice_object = slice(3) 
+print(py_string[slice_object])  # Pyt
+
+# start = 1, stop = 6, step = 2
+# contains 1, 3 and 5 indices
+slice_object = slice(1, 6, 2)
+print(py_string[slice_object])   # yhn
+
+ответ: Pyt
+       yhn
 ...
 
 ...
-3) def palindromic(sequence):
-    """Возвращает True, если последовательность является палиндромом."""
-    return all(
-        n == m
-        for n, m in zip(sequence, reversed(sequence))
-    )
+3) py_list = ['P', 'y', 't', 'h', 'o', 'n']
+py_tuple = ('P', 'y', 't', 'h', 'o', 'n')
+
+# contains indices 0, 1 and 2
+slice_object = slice(3)
+print(py_list[slice_object]) # ['P', 'y', 't']
+
+# contains indices 1 and 3
+slice_object = slice(1, 5, 2)
+print(py_tuple[slice_object]) # ('y', 'h') 
+
+ответ:['P', 'y', 't']
+       ('y', 'h')
 ...
 
-...
-4) def palindromic(sequence):
-    """Возвращает True, если последовательность является палиндромом."""
-    return not any(
-        n != m
-        for n, m in zip(sequence, reversed(sequence))
-    )
-...
 
 ...
-5) class RomanNumeral:
+4) #CALCULATOR
+def calculatePerimeter(l):
+    return 4*l
 
-    """Римская цифра, представленная как строка и число."""
+# Area of Square
+def calculateArea(l):
+    return l*l
 
-    SYMBOLS = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+expression = input("Type a function: ")
 
-    def __init__(self, number):
-        self.value = number
-
-    @classmethod
-    def from_string(cls, string):
-        return cls(cls.roman_to_int(string))
-
-    @staticmethod
-    def roman_to_int(numeral):
-        total = 0
-        for symbol, next_symbol in zip_longest(numeral, numeral[1:]):
-            value = RomanNumeral.SYMBOLS[symbol]
-            next_value = RomanNumeral.SYMBOLS.get(next_symbol, 0)
-            if value < next_value:
-                value = -value
-            total += value
-        return total
+for l in range(1, 5):
+    if (expression == 'calculatePerimeter(l)'):
+        print("If length is ", l, ", Perimeter = ", eval(expression))
+    elif (expression == 'calculateArea(l)'):
+        print("If length is ", l, ", Area = ", eval(expression))
+    else:
+        print('Wrong Function')
+        break
         ...
+
+...
+5) from decimal import Decimal
+
+# normal float
+num = 2.675
+print(round(num, 2))
+
+# using decimal.Decimal (passed float as string for precision)
+num = Decimal('2.675')
+print(round(num, 2))
+
+ответ: 2,67
+       2,68
+
+...
+
+...
+6)class Polygon:
+  def __init__(polygonType):
+    print('Polygon is a ', polygonType)
+
+class Triangle(Polygon):
+  def __init__(self):
+
+    Polygon.__init__('triangle')
+    
+print(issubclass(Triangle, Polygon))
+print(issubclass(Triangle, list))
+print(issubclass(Triangle, (list, Polygon)))
+print(issubclass(Polygon, (list, Polygon)))
+
+ответ: True
+       False
+       True
+       True
+...
+
+...
+7)
